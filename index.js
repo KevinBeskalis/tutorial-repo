6c3d6057a2d702ce705f2f07d7012a4293ce00b0
Kevin Beskalis <kevinbeskalis98@gmail.com>
2021-10-07 13:51:35 +0200

    comment

---
+++ index.js
@@ -0,0 +1,15 @@
+const div = document.querySelector("#myDiv");
+
+div.style.backgroundColor = "black";
+div.style.height = "150px";
+div.style.width = "150px";
+
+div.addEventListener("mousedown", e => {
+    console.log("you are holding the mouse down");
+    div.style.backgroundColor = "red";
+})
+
+div.addEventListener("mouseup", e => {
+    console.log("you let go of the mouse");
+    div.style.backgroundColor = "black";
+})
